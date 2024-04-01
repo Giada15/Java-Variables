7f2095a14a11a20652693810661e4d0bbec7e395
Giada15 <tascheriogiada@gmail.com>
2024-04-01 11:36:28 -0400

    Practiced with 'int', 'char', 'String' data types and concatenation

---
+++ Biography.java
@@ -0,0 +1,22 @@
+public class Biography {
+
+    public static void main(String[] args) {
+        String name = "Lili";
+        int age = 25;
+        String country = "Italy";
+        String sport = "Basketball";
+        int hours = 2;
+        String game = "Checkers";
+        String subject = "Math";
+        char grade = 'A';
+
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from " + country + ".");
+
+        System.out.println("My favourite sport is " + sport + ". I play for " + hours + " hours a day.");
+
+        System.out.println("When I'm tired, I like to play " + game + ".");
+
+        System.out.println("In school, my favourite subject was " +subject+ ", I scored an " +grade+ ".");
+    }
+    
+}
